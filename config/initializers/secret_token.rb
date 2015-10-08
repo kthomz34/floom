979f1880e6b0f0f7d1593eba35c996ca1565cbf8
Kurt Thomas <kthomz34@icloud.com>
2015-10-08 22:14:31 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Floom::Application.config.secret_key_base = '56cd26a41c86f33a3451ea632a1d08a0bcd00b784731e0a9d809d065bf34dd03421b6d03a95174f88e0947afd85a0489a4364fb12174f549eda5d9c9192a5265'
