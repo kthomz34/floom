cce64cf95ed3f878ec2c3566b5d2e223648bddf0
Kurt Thomas <kthomz34@icloud.com>
2015-10-18 02:40:31 +0000

    css font

--- config/application.rb
+++ config/application.rb
@@ -21,10 +21,10 @@ module Floom
     # config.i18n.default_locale = :de
 
     # Add the fonts path
-    config.assets.path << "#{Rails.root}/app/assets/fonts"
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
 
     # Precompile additional assets
-    config.assets.precompile += %w( .svg .eot .woff .ttf )
-    
+    config.assets.precompile += %w( .svg .eot .woff .ttf .woff2 )
+
   end
 end
