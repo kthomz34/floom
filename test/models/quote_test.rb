c86514ad5740878e9af1cacfe8994a8d28ab8ec5
Kurt Thomas <kthomz34@icloud.com>
2015-12-15 02:02:23 +0000

    no fixtures

--- test/models/quote_test.rb
+++ test/models/quote_test.rb
@@ -3,6 +3,7 @@ require 'test_helper'
 class QuoteTest < ActiveSupport::TestCase
   
   test "unique_tag" do 
+  	quote = FactoryGirl.create(:quote)
   	quote = Quote.create(:author => 'Kurt Thomas', :saying => 'You only yolo once')
   	expected = 'KT#' + quote.id.to_s
   	actual = quote.unique_tag
